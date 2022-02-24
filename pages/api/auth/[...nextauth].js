5ab654a7fbecb9880df22dd731daf50db152e1cc
colbyfayock <fayock@gmail.com>
2022-02-24 14:21:07 -0500

    using package

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,31 +1,13 @@
 import NextAuth from 'next-auth';
+import { Cloudinary } from 'nextauth-cloudinary-provider';
 
 export default NextAuth({
   secret: process.env.SECRET,
   providers: [
-    // https://next-auth.js.org/v3/configuration/providers#using-a-custom-provider
-    {
-      id: 'cloudinary',
-      name: 'Cloudinary',
-      type: 'oauth',
-      wellKnown: 'https://oauth.cloudinary.com/.well-known/openid-configuration',
-      authorization: { params: { scope: 'upload asset_management' } },
-      idToken: false,
-      checks: ['pkce', 'state'],
-      profile(profile) {
-        return {
-          id: profile.sid,
-          email: profile.sub
-        }
-      },
+    Cloudinary({
       clientId: process.env.CLOUDINARY_CLIENT_ID,
       clientSecret: process.env.CLOUDINARY_CLIENT_SECRET,
-      client: {
-        // Cloudinary oAuth uses `client_secret_post` where Open ID by default uses client_secret_basic
-        // https://github.com/panva/node-openid-client/blob/main/docs/README.md#new-clientmetadata-jwks-options
-        token_endpoint_auth_method: 'client_secret_post'
-      }
-    },
+    }),
   ],
   callbacks: {
     async jwt({ token, account }) {
