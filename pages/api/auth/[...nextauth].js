4610ae4ade8d698129cef53da00d9ac05e2205f9
colbyfayock <fayock@gmail.com>
2022-02-24 10:41:11 -0500

    removing debug code

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,9 @@
-import NextAuth from 'next-auth'
-import TwitterProvider from 'next-auth/providers/twitter';
+import NextAuth from 'next-auth';
 
 export default NextAuth({
   secret: process.env.SECRET,
   debug: true,
   providers: [
-    TwitterProvider({
-      clientId: process.env.TWITTER_CLIENT_ID,
-      clientSecret: process.env.TWITTER_CLIENT_SECRET
-    }),
     // https://next-auth.js.org/v3/configuration/providers#using-a-custom-provider
     {
       id: 'cloudinary',
